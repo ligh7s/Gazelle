b614bc1efca6d2713b5bb74713ea9fbe03ae7dc5
welp-ops <44627085+welp-ops@users.noreply.github.com>
2018-11-05 05:51:52 +0000

    why am i helping a fucking weeb

--- sections/torrents/browse.php
+++ sections/torrents/browse.php
@@ -116,8 +116,8 @@ $Groups = $Search->get_groups();
 $NumResults = $Search->record_count();
 
 if (!check_perms('site_search_many')) {
-    $RealNumResults = $NumResults;
-    $NumResults = min($NumResults, SPHINX_MAX_MATCHES);
+	$RealNumResults = $NumResults;
+	$NumResults = min($NumResults, SPHINX_MAX_MATCHES);
 }
 
 $HideFilter = isset($LoggedUser['ShowTorFilter']) && $LoggedUser['ShowTorFilter'] == 0;
@@ -410,10 +410,10 @@ if ($x % 7 != 0) { // Padding
 			</tr>
 		</table>
 		<div class="submit ft_submit">
-            <span style="float: left;"><!--
-             --><?=number_format($RealNumResults)?> Results
-                <?=!check_perms('site_search_many') ? "(Showing first $NumResults matches)" : ""?>
-            </span>
+		<span style="float: left;"><!--
+			--><?=number_format($RealNumResults)?> Results
+			<?=!check_perms('site_search_many') ? "(Showing first $NumResults matches)" : ""?>
+		</span>
 			<input type="submit" value="Filter torrents" />
 			<input type="hidden" name="action" id="ft_type" value="<?=($AdvancedSearch ? 'advanced' : 'basic')?>" />
 			<input type="hidden" name="searchsubmit" value="1" />
